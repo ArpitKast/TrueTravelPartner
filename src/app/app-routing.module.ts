9ac306d84b97e43a71e92543e625d4a88d1be545
Arpit <arpit@apie.tech>
2024-12-11 21:38:35 +0530

    logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,19 @@ import { DestinationDetailsComponent } from './components/destination-details/de
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-     // {
-     //      path: 'home',
-     //      loadChildren: () =>
-     //        import('./home/home.module').then((m) => m.HomePageModule),
-     //    },
-     //    {
-     //      path: '',
-     //      redirectTo: 'home',
-     //      pathMatch: 'full',
-     //    },
-     { path: 'home', component: HomeComponent },
-     { path: 'destination/:id', component: DestinationDetailsComponent },
-     { path: '', redirectTo: '/home', pathMatch: 'full' },
+     {
+          path: '',
+          component: HomeComponent, // Set HomeComponent directly to the root path
+        },
+        {
+          path: 'destination/:id',
+          component: DestinationDetailsComponent,
+        },
+        {
+          path: '**', // Wildcard route for a 404 page if needed
+          redirectTo: '', // Redirect to the home page or another component
+          pathMatch: 'full',
+        },
 ];
 
 @NgModule({
